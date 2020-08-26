6815f5ed25d0d55d46d5be7bf0cade4fd0e46f1e
JonasBSaxild <43033098+Fr0thgar@users.noreply.github.com>
2020-08-26 11:02:02 +0200

    Update BetalingsSystem.py
    Jeg er ved at researche mulighederne iflg. betaling

--- BetalingsSystem.py
+++ BetalingsSystem.py
@@ -1,1 +1,20 @@
+from tkinter import*
+from tkinter import ttk
+import random
+import tkinter.messagebox
+from datetime import datetime
 
+class Customer:
+    
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Customer Test")
+        self.root.geometry("1350x750+0+0")
+        self.root.config(background="powder blue")
+
+if __name__ == "__main__":
+    root =  Tk()
+    application = Customer (root)
+    root.mainloop()
+
+       
